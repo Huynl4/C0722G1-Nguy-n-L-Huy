94f05af7c94178912c1fcd96ee4e2a5d8e18aac3
Huy Nờ Lờ <you@example.com>
2022-09-06 21:38:36 +0700

    nop_bai

--- module_2/src/ss2_loop_in_java/exercise/ShowPrimesLessThan100.java
+++ module_2/src/ss2_loop_in_java/exercise/ShowPrimesLessThan100.java
@@ -1,15 +1,11 @@
 package ss2_loop_in_java.exercise;
-
-import java.util.Scanner;
-
-//hiển thị 20 số nguyên tố đầu tiên
-public class ShowFirst20Primes {
+//hiển thị các số nguyên tố nhỏ hơn 100
+public class ShowPrimesLessThan100 {
     public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-        int number = 20;
+        int number = 100;
         int count = 0;
         int n = 2;
-        while (count < number) {
+        while (n < number) {
             int value = 0;
             for (int i = 2; i < n; i++) {
                 if (n % i == 0) {
