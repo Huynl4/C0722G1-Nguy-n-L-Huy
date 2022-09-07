a137ad8c45a64b8d29dc81f38fb9cc4684197033
Huy Nờ Lờ <you@example.com>
2022-09-07 20:32:40 +0700

    nop_bai

--- module_2/src/ss3_arrays_and_methods_in_java/exercise/RemoveElementFromArray.java
+++ module_2/src/ss3_arrays_and_methods_in_java/exercise/RemoveElementFromArray.java
@@ -2,6 +2,7 @@ package ss3_arrays_and_methods_in_java.exercise;
 
 import java.util.Arrays;
 import java.util.Scanner;
+
 //xóa phần tử trong mảng
 public class RemoveElementFromArray {
     public static void main(String[] args) {
@@ -11,26 +12,55 @@ public class RemoveElementFromArray {
             System.out.println("Nhập vào số phần tử của mảng: ");
             n = scanner.nextInt();
         } while (n <= 0);
-        int A[] = new int[n];
+        int arr[] = new int[n];
         System.out.println("Nhập các phần tử cho mảng: ");
         for (i = 0; i < n; i++) {
             System.out.println("Nhập phần tử thứ " + i + ": ");
-            A[i] = scanner.nextInt();
+            arr[i] = scanner.nextInt();
         }
-        System.out.println(Arrays.toString(A));
+        System.out.println(Arrays.toString(arr));
         System.out.println("nhập số nguyên x: ");
         int x = scanner.nextInt();
         for (c = i = 0; i < n; i++) {
-            if (A[i] != x) {
-                A[c] = A[i];
+            if (arr[i] != x) {
+                arr[c] = arr[i];
                 c++;
             }
         }
         n = c;
         System.out.println("mảng sau khi xóa phần tử x: " + x);
         for (i = 0; i < n; i++) {
-            System.out.print(A[i] + "\t");
+//            System.out.print(arr[i] + "\t");
         }
+        System.out.println(Arrays.toString(arr));
     }
 }
+
+
+//
+//        Scanner scanner = new Scanner(System.in);
+//        int[] array;
+//        int n;
+//        do {
+//        System.out.println("nhập vào số lượng phần tử của mảng: ");
+//        n = Integer.parseInt(scanner.nextLine());
+//        if (n < 0) {
+//        System.out.println("bạn nhập sai , xin hãy nhập lại");
+//        }
+//        } while (n < 0);
+//        array = new int[n];
+//        for (int i = 0; i < array.length; i++) {
+//        System.out.println("nhập phần tử thứ: " + i);
+//        array[i] = Integer.parseInt(scanner.nextLine());
+//        }
+//        System.out.println(Arrays.toString(array));
+//        System.out.print("nhập số cần kiểm tra: ");
+//        int number = scanner.nextInt();
+//        for (int i = 0; i < array.length; i++) {
+//        if (array[i] == (number)) {
+//        for (int j = i; j < array.length - 1; j++) {
+//        array[j] = array[j + 1];
+//        }
+//        System.out.println(Arrays.toString(array));
+//        }
 
