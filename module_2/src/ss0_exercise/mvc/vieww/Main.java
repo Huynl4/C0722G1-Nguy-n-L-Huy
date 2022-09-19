75aac310a4eab7b43a9ab3e0083e704daffa15c0
Huy Nờ Lờ <you@example.com>
2022-09-19 08:28:00 +0700

    nop_bai

--- module_2/src/ss0_exercise/mvc/vieww/Main.java
+++ module_2/src/ss0_exercise/mvc/vieww/Main.java
@@ -7,8 +7,6 @@ import ss0_exercise.mvc.controler.TeacherController;
 public class Main {
     public static void main(String[] args) {
         PersonController.menuPerson();
-        StudentController.menuStudent();
-        TeacherController.menuTeacher();
     }
 
 }
