a6c9ee1237fe46152d1e6af888504f1c072a207a
Huy Nờ Lờ <you@example.com>
2022-11-03 13:23:29 +0700

    nop_bai

--- module_3/ss9/exercise/ProductDiscountCalculator/src/main/java/DiscountServlet.java
+++ module_3/ss9/exercise/ProductDiscountCalculator/src/main/java/DiscountServlet.java
@@ -2,10 +2,10 @@ import java.io.IOException;
 @javax.servlet.annotation.WebServlet(name = "DiscountServlet", value = "/convert")
 public class DiscountServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        float ListPrice = Float.parseFloat(request.getParameter("listprice"));
-        float DiscountPercen = Float.parseFloat(request.getParameter("discountpercen"));
-        float DiscountAmount = ListPrice * DiscountPercen * 0.01f;
-        request.setAttribute("price", DiscountAmount);
+        float listPrice = Float.parseFloat(request.getParameter("listprice"));
+        float discountPercen = Float.parseFloat(request.getParameter("discountpercen"));
+        float discountAmount = listPrice * discountPercen * 0.01f;
+        request.setAttribute("price", discountAmount);
         request.getRequestDispatcher("result.jsp").forward(request, response);
     }
 
